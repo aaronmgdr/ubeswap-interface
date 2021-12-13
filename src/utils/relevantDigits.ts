012f11f53cec74e46a032fefb0c0bb18fa5e969f
Aaron <aaron@clabs.co>
2021-12-13 11:14:15 -0800

    more precises

--- src/utils/relevantDigits.ts
+++ src/utils/relevantDigits.ts
@@ -1,11 +1,16 @@
 import { TokenAmount } from '@ubeswap/sdk'
 
 export function relevantDigits(tokenAmount?: TokenAmount) {
-  if (!tokenAmount) {
-    return '0.00'
+  if (!tokenAmount || tokenAmount.equalTo('0')) {
+    return '0.0000'
   }
+
+  if (tokenAmount.lessThan('0.0001')) {
+    return '< 0.0001'
+  }
+
   if (tokenAmount.lessThan('1')) {
-    return tokenAmount.toSignificant(6)
+    return tokenAmount.toSignificant(4)
   }
 
   if (tokenAmount.lessThan('100')) {
